1f7bab60b77714f629d69a55743c80f03a052720
Coniix <conor-evans@hotmail.com>
2023-03-15 14:01:17 +0100

    Spring pre CRUDrepository

--- src/main/kotlin/com/example/demo/SpringBootDemoApplication.kt
+++ src/main/kotlin/com/example/demo/SpringBootDemoApplication.kt
@@ -2,11 +2,12 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.stereotype.Service
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.web.bind.annotation.*
+import java.util.*
+import org.springframework.jdbc.core.query
+
 
 @Service
 class MessageService(val db: JdbcTemplate) {
@@ -14,9 +15,14 @@ class MessageService(val db: JdbcTemplate) {
 		Message(response.getString("id"), response.getString("text"))
 	}
 
+	fun findMessageById(id: String): List<Message> = db.query("select * from messages where id = ?", id) { response, _ ->
+		Message(response.getString("id"), response.getString("text"))
+	}
+
 	fun save(message: Message){
+		val id = message.id ?: UUID.randomUUID().toString()
 		db.update("insert into messages values ( ?, ? )",
-			message.id, message.text)
+			id, message.text)
 	}
 }
 
@@ -28,13 +34,18 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class MessageController {
+class MessageController(val service: MessageService) {
 	@GetMapping("/")
-	fun index() = listOf(
-		Message("1", "Hello!"),
-		Message("2", "Bonjour!"),
-		Message("3", "Privet!"),
-	)
+	fun index(): List<Message> = service.findMessages()
+
+	@GetMapping("/{id}")
+	fun index(@PathVariable id: String): List<Message> =
+		service.findMessageById(id)
+
+	@PostMapping("/")
+	fun post(@RequestBody message: Message) {
+		service.save(message)
+	}
 }
 
 data class Message(val id: String?, val text: String)
